878c8be7517f63bfc05f901120bb541a46327987
azizios011 <hdriaziz699@gmail.com>
2023-05-30 07:08:09 -1100

    Copy - Cut - Paste

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+
+print("First 3 letters:", word_first_3)
+print("Last 2 letters:", word_last_2)
+print("Middle word:", middle_word)
