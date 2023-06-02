5c4d4966238d4de78d0972a7c2b41737cb55b15a
azizios011 <hdriaziz699@gmail.com>
2023-06-02 08:12:23 -1100

    I sometimes suffer from insomnia. And when I cant fall as eep, I play what I call the alphabet game

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-output = ""
-
-for i in range(97, 123):
-    output += chr(i)
-
-print("{}{}".format(output, "\n"))
+for c in range(97, 123):
+    print("{}".format(chr(c)), end="")
