584bb0207e8fca637462b3b8c932e4f9c8e5730d
azizios011 <hdriaziz699@gmail.com>
2023-06-05 00:45:14 -1100

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
-
-a = 1
-b = 2
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
