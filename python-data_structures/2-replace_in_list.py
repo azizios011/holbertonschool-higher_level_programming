9bd2a9b0baadf48c769ebe8fd75b57227474de1b
azizios011 <hdriaziz699@gmail.com>
2023-06-12 02:18:32 -1100

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-
-    if 0 <= idx < len(my_list):
-
-        my_list[idx] = element
-
-    return my_list
+    for i in range(len(my_list)):
+        if idx < 0 or idx >= len(my_list):
+            return (my_list)
+        else:
+            my_list[idx] = element
+            return (my_list)
