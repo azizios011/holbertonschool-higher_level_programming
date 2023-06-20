760a123f022ee7ad5f7170b6fde03564b44579d1
azizios011 <hdriaziz699@gmail.com>
2023-06-20 06:49:46 -1100

    Unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
-    unique_nums = set()
-
-
-    for num in my_list:
-
-        if num not in unique_nums:
-
-            unique_nums.add(num)
-
-    result = sum(unique_nums)
-
-    return result
+    sum = 0
+    if len(my_list) > 0:
+        for i in range(0, len(my_list)):
+            test = 0
+            for j in range(0, i):
+                if my_list[i] == my_list[j]:
+                    test = 1
+            if test == 0:
+                sum += my_list[i]
+    return sum
