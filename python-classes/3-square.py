81846efc1b30b03f7f7f01a4b34d0cbf3eeb7b12
azizios011 <hdriaziz699@gmail.com>
2023-06-27 11:23:32 -1100

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class in python"""
+
+
+class Square:
+    """Represents an empty square."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
