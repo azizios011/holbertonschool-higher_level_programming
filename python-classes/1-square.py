3fefca22645b10dd82aa93da750d9253e2e99399
azizios011 <hdriaziz699@gmail.com>
2023-06-27 05:56:37 -1100

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size):
+        """Initialize a square with the given size."""
+        self.__size = size
