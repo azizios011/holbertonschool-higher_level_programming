23a616e542f2bfccee561334e2e6ffd88fb5db2e
azizios011 <hdriaziz699@gmail.com>
2023-07-04 03:46:26 -1100

    Area and Perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Set the width of the rectangle"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Set the height of the rectangle"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -40,8 +40,10 @@ class Rectangle:
 
     def area(self):
         """Return the area of the rectangle"""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """Return the perimeter of the rectangle"""
-        return 2 * (self.width + self.height)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
