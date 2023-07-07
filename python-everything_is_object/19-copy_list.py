d60955105a36f9b246cf9a959c2462d550f18b23
azizios011 <hdriaziz699@gmail.com>
2023-07-07 02:26:37 -1100

    Copy a list object

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-"""copy_list"""
-
-
 def copy_list(a_list):
-    """copy_list"""
-
-    copy = []
-    for element in a_list:
-        copy.append(element)
-    return copy
+    return a_list[:]
