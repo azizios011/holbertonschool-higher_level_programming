a2a622eef04175b9b09f2dae1089bc54f9fad462
azizios011 <hdriaziz699@gmail.com>
2023-07-09 01:17:40 -1100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module for read_file method"""
+
+
 def read_file(filename=""):
+        """Reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding='utf-8') as file:
         for line in file:
             print(line, end='')
