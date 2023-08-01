697e33f1bb867aaebcfafff1dbbd28aa53c0bd3e
azizios011 <hdriaziz699@gmail.com>
2023-08-01 02:04:17 -1100

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
--- a script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT ts.title, tg.genre_id FROM tv_shows AS ts INNER JOIN tv_show_genres AS tg ON ts.id = tg.show_id ORDER BY ts.title, tg.genre_id ASC;
