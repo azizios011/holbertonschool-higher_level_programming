60c6054797784a370ca5beb12064e7cd61d02056
azizios011 <hdriaziz699@gmail.com>
2023-08-01 02:13:38 -1100

    Only Comedy

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+--  a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title FROM tv_show_genres RIGHT JOIN tv_shows ON show_id = tv_shows.id RIGHT JOIN tv_genres ON genre_id = tv_genres.id WHERE name = "Comedy" ORDER BY tv_shows.title;
