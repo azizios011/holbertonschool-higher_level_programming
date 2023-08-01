a15f4fa66907f446c2384347c25b4b118d493441
azizios011 <hdriaziz699@gmail.com>
2023-08-01 01:35:55 -1100

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- Check if the user user_0d_1 already exists
-SELECT 1 FROM mysql.user WHERE user = 'user_0d_1';
-
--- If the user does not exist, create it and grant all privileges
-IF NOT FOUND_ROWS() THEN
-  CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-  GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
-  FLUSH PRIVILEGES;
-END IF;
+-- script that creates the MySQL server user user_0d_1.
+create user if not exists 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT all privileges on *.* to 'user_0d_1'@'localhost';
+FLUSH privileges;
