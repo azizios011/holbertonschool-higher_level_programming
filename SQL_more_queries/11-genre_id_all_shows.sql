ebe557b0add41b3fe6d6f3507ba4e13745750ae8
azizios011 <hdriaziz699@gmail.com>
2023-08-01 02:05:55 -1100

    Genre ID for all shows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+--  a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT ts.title, tg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tg ON ts.id = tg.show_id ORDER BY ts.title, tg.genre_id ASC;
