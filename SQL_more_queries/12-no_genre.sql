76436149a50d058b8513bfc2d64bdcd760ded5bc
azizios011 <hdriaziz699@gmail.com>
2023-08-01 02:07:30 -1100

    No genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+--  a script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT ts.title, tg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tg ON ts.id = tg.show_id WHERE tg.genre_id IS NULL ORDER BY ts.title, tg.genre_id ASC;
