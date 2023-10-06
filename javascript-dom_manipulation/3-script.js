da88a290480a7643a0a8cffa9ded1fbc025c04cf
azizios011 <hdriaziz699@gmail.com>
2023-10-06 12:53:38 +0100

    Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,17 @@
+// declare a constant varaible that get an elements by id.
+const element = document.getElementById("toggle_header");
+// declare a constant varaible that select an elements.
+const elements = document.querySelector("header");
+// this method will work when the user click on the "toggle_header(id)".
+element.addEventListener("click", function() {
+    // check if the header element(elements) currently has the "red" class. If it does, we remove the "red" class and add the "green" class.
+    if (elements.classList.contains("red")){
+    elements.classList.remove("red");
+    elements.classList.add("green");
+    }
+    else {
+        // If it doesn't have the "red" class, we remove the "green" class and add the "red" class.
+        elements.classList.remove("green");
+        elements.classList.add("red");
+    }
+});
