1b82fd657db593ff5a1794a90967f54997e23fda
azizios011 <hdriaziz699@gmail.com>
2023-10-06 11:06:43 +0100

    Color Me

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,2 +1,5 @@
+// define a constant variable that select a specific elements
 const elements = document.querySelector("header");
-elements.style.color = "red"
+// change the element style color to a red
+elements.style.color = "#FF0000"
+
